c32f3e5e9a5c451f13a3120cce85d8062a052116
D-cyber-web <muthama3david@gmail.com>
2023-11-02 23:41:13 +0300

    created components

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -5,6 +5,8 @@ export const Navbar = () => {
     return (
         <div className='navbar'>
             <h2>this is my navbar</h2>
+            
+            <h1>welcome to javascript on frontend</h1>
         </div>
     )
 }
